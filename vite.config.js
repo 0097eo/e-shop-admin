3c72c28c5c087596baf6bb084c23fcd4e990ef27
0097eo <emmanuelokello294@gmail.com>
2025-04-29 11:06:22 +0300

    chore: add testing dependencies and configure Vitest with jsdom environment

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,15 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./test/setup.js'],
+    coverage: {
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      reporter: ['text', 'json', 'html'],
+    }
+  },
   server: {
     proxy: {
       '/api': {
